0a1cbfc9f084dc262f7c7e93021bdc974efa371f
Marcos Junior <marcos.acj@outlook.com>
2019-08-19 23:06:05 -0300

    Classe Contato

---
+++ src/main/java/com/jsf/model/Contato.java
@@ -0,0 +1,72 @@
+package com.jsf.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table( name = "contatos" )
+public class Contato {
+	
+	@Id
+	@SequenceGenerator(name = "seq_contato", initialValue = 1, allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "seq_contato")
+	private Integer id;
+	
+	@Column(name = "ativo")
+	private boolean ativo = true;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "telefone")
+	private String telefone;
+	
+	@Column(name = "email")
+	private String email;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+}
